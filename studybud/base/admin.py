af0edc9001eda4f58b007033c9bf9b8087cde524
Shih Chiang <shih91@gmail.com>
2021-10-25 11:24:50 +0800

    avatar added

--- studybud/base/admin.py
+++ studybud/base/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room, Topic, Message, Reply
+from .models import Room, Topic, Message, Reply, User
 
+admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
