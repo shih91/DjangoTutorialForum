522f070a926a97f914896dd78610fa73ba99efd5
Shih Chiang <shih91@gmail.com>
2021-12-13 15:10:09 +0800

    logging

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*.herokuapp.com'
+    '*'
 ]
 
 
@@ -144,3 +144,37 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 import django_on_heroku
 django_on_heroku.settings(locals())
+
+import logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'studybud': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
