73af7c9c5db8f0872826632d700bfac85e9abb89
Shih Chiang <shih91@gmail.com>
2021-12-13 13:17:04 +0800

    Remove a hardcoded Django secret key

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-to22whs2=7n+*-&ff1(*puy-8ld=&xh4!fr-5q0m6@mcfz_tv7'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
